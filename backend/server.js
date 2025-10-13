047d2151784a17df46ff792c977e2122f7e7b629
tashindudasanayake <tashindudasanayake2@gmail.com>
2025-10-14 00:20:43 +0530

    Add product routes to the server

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import productRoutes from "./routes/productRoutes.js";
 
 
 dotenv.config();
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)
